8ae71695ddfb90b813df6079d98e37cd5c0c83b2
Baris Sariusta <B.Sariusta@student.han.nl>
2025-07-07 18:54:36 +0200

    remove unused policy methods for now

--- app/Contracts/ApiController.php
+++ app/Contracts/ApiController.php
@@ -14,25 +14,4 @@ abstract class ApiController
     use ApiResponses;
     use AuthorizesRequests;
     use ValidatesRequests;
-
-    protected ApiController $policyClass;
-
-    public function include(string $relationship) : bool
-    {
-        $param = request()->get('include');
-
-        if (! isset($param))
-        {
-            return false;
-        }
-
-        $includeValues = explode(',', mb_strtolower($param));
-
-        return in_array(mb_strtolower($relationship), $includeValues);
-    }
-
-    public function isAble($ability, $targetModel)
-    {
-        return $this->authorize($ability, [$targetModel, $this->policyClass]);
-    }
 }
