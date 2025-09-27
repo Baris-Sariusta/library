c2e4c8a20ff253bc30b09a4b95b5dd12b15e1211
Baris Sariusta <B.Sariusta@student.han.nl>
2025-09-27 12:36:35 +0200

    Added tests for loan status enum

--- tests/Unit/Enums/LoanStatusTest.php
+++ tests/Unit/Enums/LoanStatusTest.php
@@ -2,6 +2,18 @@
 
 declare(strict_types=1);
 
-todo('should construct a LoanStatus enum');
+use App\Enums\LoanStatus;
 
-todo('should have the functions in the enum');
+it('should construct a LoanStatus enum', function (LoanStatus $status) : void
+{
+    expect($status)
+        ->toBeInstanceOf(LoanStatus::class)
+        ->name->toBeString()
+        ->value->toBeString();
+})->with(LoanStatus::cases());
+
+it('should have the functions in the enum', function () : void
+{
+    expect(LoanStatus::ONGOING)->description()->toBe('Ongoing');
+    expect(LoanStatus::RETURNED)->description()->toBe('Returned');
+});
