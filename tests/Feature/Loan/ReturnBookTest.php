fe6c017c30129939d9177722f4340d1bc30244be
Baris Sariusta <B.Sariusta@student.han.nl>
2025-09-29 15:14:42 +0200

    add feature test and add helpers in LoanFactory.php

---
+++ tests/Feature/Loan/ReturnBookTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Enums\LoanStatus;
+use App\Models\Loan;
+use App\Models\User;
+use App\Enums\UserRole;
+
+test('that a user can return a book', function () : void
+{
+    $user = User::factory()
+        ->withRole(UserRole::MEMBER)
+        ->create();
+
+    $loan = Loan::factory()
+        ->for($user)
+        ->asOngoing()
+        ->create();
+
+    $this->actingAs($user)
+        ->patch(
+            uri: '/api/loans',
+            data: [],
+        )
+        ->assertOk();
+
+    $this->assertDatabaseHas(
+        table: Loan::getTableName(),
+        data: [
+            'id' => $loan->id,
+            'status' => LoanStatus::RETURNED,
+        ]);
+});
