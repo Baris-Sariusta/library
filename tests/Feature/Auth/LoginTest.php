641697d2cadbbe79dd0d947dcec22825a804b2f3
Baris Sariusta <B.Sariusta@student.han.nl>
2025-10-10 12:54:20 +0200

    add model test for generating api token

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -5,16 +5,20 @@ declare(strict_types=1);
 use App\Models\User;
 use Laravel\Sanctum\Sanctum;
 
-it('fails to log in if user is already authenticated', function () {
+it('fails to log in if a user is already authenticated', function ()
+{
     $user = User::factory()->create();
 
+    // Make sure the user is authenticated...
     Sanctum::actingAs($user);
 
+    // Create a login attempt with the already authenticated user...
     $response = $this->postJson('/api/login', [
         'email' => $user->email,
         'password' => 'password',
     ]);
 
+    // Make sure an error is returned...
     $response->assertStatus(409)
         ->assertJson(['message' => 'You are already logged in.']);
 });
